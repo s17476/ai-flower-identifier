311ab6a852681af94d2bf5d7f78e9de0fc77b3c2
Grzegorz Fraczek <gf@lnasoftware.com>
2023-03-20 10:59:02 +0100

    ImagePicker added

--- AI Flower Identifier/ViewController.swift
+++ AI Flower Identifier/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
+        
     }
 
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+//MARK: - UIImagePickerControllerDelegate Methods
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+                
+            guard let ciImage = CIImage(image: pickedImage) else {
+                fatalError("Cannot convert to CIImage")
+            }
+            
+            imageView.image = pickedImage
+            
+        }
+        
+        
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(inage: CIImage) {
+        
+    }
 }
 
